fd9e55b0570d3b86f21adc20a73d903dbb182f38
iansteyn <iansteyn03@gmail.com>
2026-07-27 14:07:25 -0700

    Add italics to Google Fonts link for IBM Plex Mono and Inter

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" media="all">
 <link rel="preconnect" href="https://fonts.googleapis.com"> 
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;700&family=Inter:wght@400;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Inter:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=IBM+Plex+Mono:wght@400;700&family=Inter:wght@400;700&display=swap" rel="stylesheet">
 
 <?php wp_head(); ?>
